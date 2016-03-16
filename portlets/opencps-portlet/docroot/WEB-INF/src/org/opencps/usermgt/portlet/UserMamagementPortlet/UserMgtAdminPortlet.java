16ae66ba989290c785439071178586e09cba6268
trungnt-ksnb <trungnt@fds.vn>
2016-03-16 14:53:28 +0700

    Change package org.opencps.usermgt.portlet.UserManagementPortlet to org.opencps.usermgt.portlet Rename clss UserMgtAdminPortlet to UserMgtPortlet

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/usermgt/portlet/UserMamagementPortlet/UserMgtAdminPortlet.java
+++
@@ -1,8 +0,0 @@
-package org.opencps.usermgt.portlet.UserMamagementPortlet;
-
-import com.liferay.util.bridges.mvc.MVCPortlet;
-
-
-public class UserMgtAdminPortlet extends MVCPortlet{
-
-}
---
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/usermgt/portlet/UserMgtPortlet.java
@@ -0,0 +1,8 @@
+package org.opencps.usermgt.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+
+public class UserMgtPortlet extends MVCPortlet{
+
+}
