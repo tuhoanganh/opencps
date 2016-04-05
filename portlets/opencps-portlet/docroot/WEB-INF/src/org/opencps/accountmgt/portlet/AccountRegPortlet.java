38044a415cdf38d6d9aef6f15bade77e0a2f1fe2
trungnt-ksnb <trungnt@fds.vn>
2016-04-05 16:51:10 +0700

    Fix conflict

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/accountmgt/portlet/AccountRegPortlet.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/accountmgt/portlet/AccountRegPortlet.java
@@ -31,18 +31,16 @@ import org.opencps.accountmgt.service.CitizenLocalServiceUtil;
 import org.opencps.datamgt.model.DictItem;
 import org.opencps.datamgt.service.DictItemLocalServiceUtil;
 import org.opencps.util.DateTimeUtil;
-<<<<<<< HEAD
-=======
+
 import org.opencps.util.MessageBusUtil;
 import org.opencps.util.MessageKeys;
 import org.opencps.util.PortletPropsValues;
->>>>>>> FETCH_HEAD
+
 import org.opencps.util.PortletUtil;
 import org.opencps.util.WebKeys;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.MessageBusUtil;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
@@ -226,7 +224,7 @@ public class AccountRegPortlet extends MVCPortlet {
 				            .getYear(),
 				        repositoryId, sourceFileName, contentType, title,
 				        inputStream, size, serviceContext);
-				
+
 				if (business != null) {
 					User mappingUser = UserLocalServiceUtil
 					    .getUser(business
@@ -244,24 +242,48 @@ public class AccountRegPortlet extends MVCPortlet {
 		}
 
 		catch (Exception e) {
-			if(e instanceof DuplicateBusinessEmailException) {
-				SessionErrors.add(actionRequest, DuplicateBusinessEmailException.class);
-			} else if(e instanceof OutOfLengthBusinessEmailException) {
-				SessionErrors.add(actionRequest, OutOfLengthBusinessEmailException.class);
-			} else if(e instanceof OutOfLengthBusinessNameException) {
-				SessionErrors.add(actionRequest, OutOfLengthBusinessNameException.class);
-			} else if (e instanceof OutOfLengthBusinessEnNameException) {
-				SessionErrors.add(actionRequest, OutOfLengthBusinessEnNameException.class);
-			} else if(e instanceof OutOfLengthBusinessShortNameException) {
-				SessionErrors.add(actionRequest, OutOfLengthBusinessShortNameException.class);
-			} else if(e instanceof OutOfLengthBusinessRepresentativeNameException) {
-				SessionErrors.add(actionRequest, OutOfLengthBusinessRepresentativeNameException.class);
-			} else if(e instanceof OutOfLengthBusinessRepresentativeRoleException) {
-				SessionErrors.add(actionRequest, OutOfLengthBusinessRepresentativeRoleException.class);
-			} else if(e instanceof OutOfLengthCitizenAddressException) {
-				SessionErrors.add(actionRequest, OutOfLengthCitizenAddressException.class);
-			} else {
-				_log.error(e);
+			if (e instanceof DuplicateBusinessEmailException) {
+				SessionErrors
+				    .add(actionRequest, DuplicateBusinessEmailException.class);
+			}
+			else if (e instanceof OutOfLengthBusinessEmailException) {
+				SessionErrors
+				    .add(
+				        actionRequest, OutOfLengthBusinessEmailException.class);
+			}
+			else if (e instanceof OutOfLengthBusinessNameException) {
+				SessionErrors
+				    .add(actionRequest, OutOfLengthBusinessNameException.class);
+			}
+			else if (e instanceof OutOfLengthBusinessEnNameException) {
+				SessionErrors
+				    .add(
+				        actionRequest,
+				        OutOfLengthBusinessEnNameException.class);
+			}
+			else if (e instanceof OutOfLengthBusinessShortNameException) {
+				SessionErrors
+				    .add(
+				        actionRequest,
+				        OutOfLengthBusinessShortNameException.class);
+			}
+			else if (e instanceof OutOfLengthBusinessRepresentativeNameException) {
+				SessionErrors
+				    .add(
+				        actionRequest,
+				        OutOfLengthBusinessRepresentativeNameException.class);
+			}
+			else if (e instanceof OutOfLengthBusinessRepresentativeRoleException) {
+				SessionErrors
+				    .add(
+				        actionRequest,
+				        OutOfLengthBusinessRepresentativeRoleException.class);
+			}
+			else if (e instanceof OutOfLengthCitizenAddressException) {
+				SessionErrors
+				    .add(
+				        actionRequest,
+				        OutOfLengthCitizenAddressException.class);
 			}
 			else {
 				SessionErrors
@@ -413,23 +435,7 @@ public class AccountRegPortlet extends MVCPortlet {
 
 		}
 		catch (Exception e) {
-<<<<<<< HEAD
-			    if(e instanceof OutOfLengthCitizenAddressException) {
-			    	SessionErrors.add(actionRequest, 
-			    		OutOfLengthCitizenAddressException.class);
-			    } else if(e instanceof OutOfLengthCitizenEmailException) {
-			    	SessionErrors.add(actionRequest, 
-			    		OutOfLengthCitizenEmailException.class);
-			    } else if(e instanceof OutOfLengthCitizenNameException) {
-			    	SessionErrors.add(actionRequest, 
-			    		OutOfLengthCitizenNameException.class);
-			    } else if(e instanceof DuplicateCitizenEmailException) {
-			    	SessionErrors.add(actionRequest, 
-			    		DuplicateCitizenEmailException.class);
-			    } else {
-			    	_log.error(e);
-			    }
-=======
+
 			if (e instanceof OutOfLengthCitizenAddressException) {
 				SessionErrors
 				    .add(
@@ -456,10 +462,10 @@ public class AccountRegPortlet extends MVCPortlet {
 			}
 			_log
 			    .error(e);
->>>>>>> FETCH_HEAD
+
 		}
 		finally {
-			
+
 		}
 	}
 
