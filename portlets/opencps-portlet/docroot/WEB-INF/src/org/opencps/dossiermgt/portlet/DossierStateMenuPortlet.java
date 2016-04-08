0bfbd07685cb607c6d99a9faa883bd760046e1f5
trungnt-ksnb <trungnt@fds.vn>
2016-04-08 14:51:06 +0700

    Update definition portlet

---
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/dossiermgt/portlet/DossierStateMenuPortlet.java
@@ -0,0 +1,11 @@
+package org.opencps.dossiermgt.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class DossierStateMenuPortlet
+ */
+public class DossierStateMenuPortlet extends MVCPortlet {
+ 
+
+}
