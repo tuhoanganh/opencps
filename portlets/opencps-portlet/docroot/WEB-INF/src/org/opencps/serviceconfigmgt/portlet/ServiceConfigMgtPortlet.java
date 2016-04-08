c3943537acf1250c1e566624ed1f449eb3e51ea5
trungnt-ksnb <trungnt@fds.vn>
2016-04-08 13:59:45 +0700

    Create New Portlet

---
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/serviceconfigmgt/portlet/ServiceConfigMgtPortlet.java
@@ -0,0 +1,11 @@
+package org.opencps.serviceconfigmgt.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ServiceConfigMgtPortlet
+ */
+public class ServiceConfigMgtPortlet extends MVCPortlet {
+ 
+
+}
