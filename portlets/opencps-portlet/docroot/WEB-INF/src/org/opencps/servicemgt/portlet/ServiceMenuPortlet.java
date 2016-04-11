983b38bccf9cfe86127ec9d8893f1e4f507276f2
Khoa Hạnh Phúc <khoavd@fds.vn>
2016-04-11 16:49:02 +0700

    Update ServiceMgt

---
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/servicemgt/portlet/ServiceMenuPortlet.java
@@ -0,0 +1,29 @@
+/**
+ * OpenCPS is the open source Core Public Services software
+ * Copyright (C) 2016-present OpenCPS community
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
+package org.opencps.servicemgt.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+
+/**
+ * @author khoavd
+ *
+ */
+public class ServiceMenuPortlet extends MVCPortlet{
+
+}
