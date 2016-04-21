c01a4b3b9951907efcb0eb5238c0052bb1f77123
Khoa Hạnh Phúc <khoavd@fds.vn>
2016-04-21 13:48:30 +0700

    Update ServiceFinder

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/processmgt/service/persistence/ServiceProcessFinderImpl.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/processmgt/service/persistence/ServiceProcessFinderImpl.java
@@ -1,5 +1,5 @@
 /**
- * OpenCPS is the open source Core Public Services software Copyright (C)
+O * OpenCPS is the open source Core Public Services software Copyright (C)
  * 2016-present OpenCPS community This program is free software: you can
  * redistribute it and/or modify it under the terms of the GNU Affero General
  * Public License as published by the Free Software Foundation, either version 3
