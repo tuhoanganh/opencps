db0fc6e8987f2b043147e9f20f05957ae62de176
trungnt-ksnb <trungnt@fds.vn>
2016-04-24 14:37:24 +0700

    Definition add Dossier method

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/dossiermgt/service/impl/DossierLocalServiceImpl.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/dossiermgt/service/impl/DossierLocalServiceImpl.java
@@ -1,4 +1,3 @@
-
 /*******************************************************************************
  * OpenCPS is the open source Core Public Services software
  * Copyright (C) 2016-present OpenCPS community
@@ -16,20 +15,21 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 
-
 package org.opencps.dossiermgt.service.impl;
 
+import org.opencps.dossiermgt.model.Dossier;
 import org.opencps.dossiermgt.service.base.DossierLocalServiceBaseImpl;
 
+import com.liferay.portal.service.ServiceContext;
+
 /**
- * The implementation of the dossier local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link org.opencps.dossiermgt.service.DossierLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
+ * The implementation of the dossier local service. <p> All custom service
+ * methods should be put in this class. Whenever methods are added, rerun
+ * ServiceBuilder to copy their definitions into the
+ * {@link org.opencps.dossiermgt.service.DossierLocalService} interface. <p>
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM. </p>
  *
  * @author trungnt
  * @see org.opencps.dossiermgt.service.base.DossierLocalServiceBaseImpl
@@ -37,8 +37,19 @@ import org.opencps.dossiermgt.service.base.DossierLocalServiceBaseImpl;
  */
 public class DossierLocalServiceImpl extends DossierLocalServiceBaseImpl {
 	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this interface directly. Always use {@link org.opencps.dossiermgt.service.DossierLocalServiceUtil} to access the dossier local service.
+	 * NOTE FOR DEVELOPERS: Never reference this interface directly. Always use
+	 * {@link org.opencps.dossiermgt.service.DossierLocalServiceUtil} to access
+	 * the dossier local service.
 	 */
+
+	public Dossier addDossier(
+	    long userId, long dossierId, long serviceInfoId, String cityCode,
+	    String districtCode, String wardCode, String subjectName,
+	    String subjectID, String address, String contactName,
+	    String contactTelNo, String contactEmail, String note,
+	    ServiceContext serviceContext) {
+
+		return null;
+	}
+
 }
