74a1081af613108710ff92816b4ed4039cadc5cb
dunglt <dunglt@localhost.localdomain>
2016-05-10 14:16:31 +0700

    usermgt edit find list jobpos by groupId and WorkingunitID

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/usermgt/service/impl/JobPosLocalServiceImpl.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/usermgt/service/impl/JobPosLocalServiceImpl.java
@@ -241,7 +241,17 @@ public class JobPosLocalServiceImpl extends JobPosLocalServiceBaseImpl {
 
 		return jobPosPersistence.findByG_W(groupId, workingUnitId);
 	}
+	
+	public List<JobPos> getJobPossG_W(long groupId, long workingUnitId, int start,
+		int end, OrderByComparator orderByComparator)
+					throws SystemException {
 
+				return jobPosPersistence.findByG_W(groupId, workingUnitId, start, end, orderByComparator);
+			}
+	
+	public int countJobPosG_W(long groupId, long workingUnitId) throws SystemException {
+		return jobPosPersistence.countByG_W(groupId, workingUnitId);
+	}
 	public List<JobPos> getJobPoss(long groupId, long workingUnitId,
 			long directWorkingUnitId) throws SystemException {
 
