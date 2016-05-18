5f8de24330cd8df6a7488adb6c43ef912d4a1820
trungnt-ksnb <trungnt@fds.vn>
2016-05-18 11:00:17 +0700

    Change package name

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/jasperreport/datasource/JRJSONDataSource.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/jasperreport/datasource/JRJSONDataSource.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>
 */
 
-package org.opencps.report.datasource;
+package org.opencps.jasperreport.datasource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
