3bec30c55f2e4e469f7b20b7e81ae321ccf344de
trungnt-ksnb <trungnt@fds.vn>
2016-06-02 14:24:07 +0700

    Update main.js

--- portlets/opencps-portlet/docroot/js/main.js
+++ portlets/opencps-portlet/docroot/js/main.js
@@ -1,5 +1,5 @@
 Liferay.provide(window, 'openDialog', function(uri, id, title) {
-	var dossierFileDialog = Liferay.Util.openWindow(
+	var opencpsDialog = Liferay.Util.openWindow(
 		{
 			dialog: {
 				cache: false,
@@ -16,4 +16,38 @@ Liferay.provide(window, 'openDialog', function(uri, id, title) {
 			
 		}
 	);
+});
+
+
+Liferay.provide(window, 'uploadDossierFile', function(e, uri, ns) {
+	
+	var A = AUI();
+
+	var instance = A.one(e);
+	
+	var dossierId = instance.attr('dossier');
+	
+	var dossierPartId = instance.attr('dossier-part');
+	
+	var dossierFileId = instance.attr('dossier-file');
+	
+	var fileGroupId = instance.attr('file-group');
+	
+	var groupName = instance.attr('group-name');
+	
+	//Create render url
+	var portletURL = Liferay.PortletURL.createURL(uri);
+	portletURL.setParameter("dossierId", dossierId);
+	portletURL.setParameter("dossierPartId", dossierPartId);
+	portletURL.setParameter("groupName", groupName);
+	portletURL.setParameter("dossierFileId", dossierFileId);
+	portletURL.setParameter("fileGroupId", fileGroupId);
+	portletURL.setParameter("modalDialogId", "upload-dossier-file");
+	
+	//Open dialog
+	openDialog(uri, ns + 'upload-dossier-file', Liferay.Language.get("upload-dossier-file"));
+});
+
+Liferay.provide((window, 'dynamicForm'), function(e, uri, ns){
+	var A = AUI();
 });
