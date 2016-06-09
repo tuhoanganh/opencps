9b0b23b67e714a46dd5592b7e7379be1bdccac48
trungnt-ksnb <trungnt@fds.vn>
2016-06-09 14:48:34 +0700

    Format coding style

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/dossiermgt/search/DossierFileSearch.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/dossiermgt/search/DossierFileSearch.java
@@ -27,7 +27,6 @@ import javax.portlet.PortletURL;
 
 import org.opencps.datamgt.util.DataMgtUtil;
 import org.opencps.dossiermgt.model.DossierFile;
-import org.opencps.util.DateTimeUtil;
 
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.log.Log;
@@ -35,44 +34,49 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.ParamUtil;
 
-
 public class DossierFileSearch extends SearchContainer<DossierFile> {
+
 	static List<String> headerNames = new ArrayList<String>();
 	static Map<String, String> orderableHeaders = new HashMap<String, String>();
 	static {
-		headerNames.add("no");
-		//headerNames.add("create-date");
-		headerNames.add("dossierfile-fileno");
-		headerNames.add("dossierfile-filedate");
-		headerNames.add("dossierfile-displayname");
-		headerNames.add("govagency-name");
-		headerNames.add("owner-name");
-		//headerNames.add("dossierfile-filetype");
+		headerNames
+			.add("#");
+		// headerNames.add("create-date");
+		headerNames
+			.add("dossierfile-fileno");
+		headerNames
+			.add("dossierfile-filedate");
+		headerNames
+			.add("dossierfile-displayname");
+		headerNames
+			.add("govagency-name");
+		headerNames
+			.add("owner-name");
+		// headerNames.add("dossierfile-filetype");
 	}
 	public static final String EMPTY_RESULTS_MESSAGE =
-	    "no-dossierfile-were-found";
+		"no-dossierfile-were-found";
 
 	public DossierFileSearch(
-	    PortletRequest portletRequest, int delta, PortletURL iteratorURL) {
+		PortletRequest portletRequest, int delta, PortletURL iteratorURL) {
 
 		super(
-		    portletRequest, new DossierFileDisplayTerms(
-		        portletRequest), new DossierFileSearchTerms(
-		            portletRequest), DEFAULT_CUR_PARAM, delta, iteratorURL, 
-		    	headerNames, EMPTY_RESULTS_MESSAGE);
+			portletRequest, new DossierFileDisplayTerms(
+				portletRequest), new DossierFileSearchTerms(
+					portletRequest), DEFAULT_CUR_PARAM, delta, iteratorURL, headerNames, EMPTY_RESULTS_MESSAGE);
 
 		DossierFileDisplayTerms displayTerms =
-		    (DossierFileDisplayTerms) getDisplayTerms();
-		
+			(DossierFileDisplayTerms) getDisplayTerms();
+
 		try {
 
 			String orderByCol = ParamUtil
-			    .getString(portletRequest, "orderByCol");
+				.getString(portletRequest, "orderByCol");
 			String orderByType = ParamUtil
-			    .getString(portletRequest, "orderByType");
+				.getString(portletRequest, "orderByType");
 
 			OrderByComparator orderByComparator = DataMgtUtil
-			    .getDictCollectionOrderByComparator(orderByCol, orderByType);
+				.getDictCollectionOrderByComparator(orderByCol, orderByType);
 
 			setOrderableHeaders(orderableHeaders);
 			setOrderByCol(orderByCol);
@@ -81,18 +85,18 @@ public class DossierFileSearch extends SearchContainer<DossierFile> {
 		}
 		catch (Exception e) {
 			_log
-			    .error(e);
+				.error(e);
 		}
 	}
 
 	public DossierFileSearch(
-	    PortletRequest portletRequest, PortletURL iteratorURL) {
+		PortletRequest portletRequest, PortletURL iteratorURL) {
 
 		this(
-		    portletRequest, DEFAULT_DELTA, iteratorURL);
+			portletRequest, DEFAULT_DELTA, iteratorURL);
 	}
 
 	private static Log _log = LogFactoryUtil
-	    .getLog(DossierFileSearch.class);
+		.getLog(DossierFileSearch.class);
 
 }
