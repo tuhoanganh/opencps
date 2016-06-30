51ef5e160997a26988e00ad03398bb34b5e79ebd
trungnt-ksnb <trungnt@fds.vn>
2016-06-30 19:06:55 +0700

    Update new opencps-pki lib

--- hooks/opencps-hook/docroot/META-INF/custom_jsps/html/extensions/opencps-pki.js
+++ hooks/opencps-hook/docroot/META-INF/custom_jsps/html/extensions/opencps-pki.js
@@ -10,17 +10,6 @@
 var digidoc_mime = 'application/x-digidoc';
 var bcy_mime = 'application/x-cryptolib05plugin';
 
-function hex2Array(hex) {
-    if(typeof hex == 'string') {
-        var len = Math.floor(hex.length / 2);
-        var ret = new Uint8Array(len);
-        for (var i = 0; i < len; i++) {
-            ret[i] = parseInt(hex.substr(i * 2, 2), 16);
-        }
-        return ret;
-    }
-}
-
 function hasPlugin(mime) {
     if(navigator.mimeTypes && mime in navigator.mimeTypes) {
         return true;
@@ -29,7 +18,7 @@ function hasPlugin(mime) {
 }
 
 function loadSignaturePlugin(mime) {
-    var element = "bcy" + mime.replace('/', '').replace('-', '');
+    var element = mime.replace('/', '').replace('-', '');
     if(document.getElementById(element)) {
         return document.getElementById(element);
     }
@@ -44,17 +33,17 @@ function loadSignaturePlugin(mime) {
 function signBcy(signer) {
     var plugin = loadSignaturePlugin(bcy_mime);
     if (plugin.valid) {
-        var code = plugin.Sign(hex2Array(signer.options.hash.hex));
+        var code = plugin.Sign(hexToBase64(signer.options.hash.hex));
         if (code === 0 || code === 7) {
-            var sign = plugin.Signature;
-            signer.options.signature.value = sign;
+            signer.options.signature.value = plugin.Signature;
             if (signer.options.afterSign) {
                 signer.options.afterSign(signer, signer.options.signature);
             }
         }
         else {
             if (signer.options.onError) {
-                signer.options.onError(signer, 'sign() failed');
+                console.log("sign() failed: " + plugin.ErrorMessage);
+                signer.options.onError(signer, 'sign() failed: ' + plugin.ErrorMessage);
             }
         }
     }
@@ -73,16 +62,16 @@ if (window.hwcrypto) {
 function signHwCrypto(signer) {
     window.hwcrypto.getCertificate({lang: 'en'}).then(function(certificate) {
         window.hwcrypto.sign(certificate, {type: signer.options.hash.type, hex: signer.options.hash.hex}, {lang: 'en'}).then(function(signature) {
-            signer.options.signature.certificate = certificate.hex;
-            signer.options.signature.value = signature.hex;
+            signer.options.signature.certificate = hexToBase64(certificate.hex);
+            signer.options.signature.value = hexToBase64(signature.hex);
             if (signer.options.afterSign) {
                 signer.options.afterSign(signer, signer.options.signature);
             }
         }, function(err) {
+            console.log("sign() failed: " + err);
             if (signer.options.onError) {
                 signer.options.onError(signer, err);
             }
-            console.log("sign() failed: " + err);
         });
     }, function(err) {
         console.log("getCertificate() failed: " + err);
@@ -132,7 +121,7 @@ $.extend({
         var cert = null;
         if (window.hwcrypto) {
             window.hwcrypto.getCertificate({lang: 'en'}).then(function(response) {
-                cert = response.hex;
+                cert = hexToBase64(response.hex);
             }, function(err) {
                 console.log("getCertificate() failed: " + err);
             });
