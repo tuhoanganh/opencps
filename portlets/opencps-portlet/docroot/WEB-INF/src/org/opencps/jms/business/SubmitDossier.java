fede3fe952889f223d747406ffb8e2013c1a02cf
trungnt-ksnb <trungnt@fds.vn>
2016-07-14 14:36:50 +0700

    Add business class SubmitDossier

---
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/jms/business/SubmitDossier.java
@@ -0,0 +1,42 @@
+/**
+ * OpenCPS is the open source Core Public Services software
+ * Copyright (C) 2016-present OpenCPS community
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
+package org.opencps.jms.business;
+
+import org.opencps.dossiermgt.model.Dossier;
+import org.opencps.jms.message.SubmitDossierMessage;
+import org.opencps.jms.message.body.DossierMsgBody;
+
+/**
+ * @author trungnt
+ */
+public class SubmitDossier {
+
+	public Dossier createDossier(SubmitDossierMessage submitDossierMessage) {
+
+		DossierMsgBody dossierMsgBody =
+			submitDossierMessage.getDossierMsgBody();
+		
+		
+
+		return null;
+	}
+
+	public void sendToBackend() {
+
+	}
+}
