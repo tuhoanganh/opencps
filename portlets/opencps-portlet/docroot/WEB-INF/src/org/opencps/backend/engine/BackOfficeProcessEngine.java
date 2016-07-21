d7066eb4e492a5f2ab0f405980214b6404e246ec
Khoa Hạnh Phúc <khoavd@fds.vn>
2016-07-21 17:55:35 +0700

    Update Payment file

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/backend/engine/BackOfficeProcessEngine.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/backend/engine/BackOfficeProcessEngine.java
@@ -271,6 +271,8 @@ public class BackOfficeProcessEngine implements MessageListener {
 				}
 
 				toBackOffice.setProcessWorkflowId(processWorkflowId);
+				toBackOffice.setCompanyId(toEngineMsg.getCompanyId());
+				toBackOffice.setGovAgencyCode(govAgencyCode);
 
 				long ownerUserId = 0;
 				long ownerOrganizationId = 0;
@@ -341,6 +343,8 @@ public class BackOfficeProcessEngine implements MessageListener {
 				toBackOffice.setDossierId(toEngineMsg.getDossierId());
 				toBackOffice.setFileGroupId(toEngineMsg.getFileGroupId());
 				toBackOffice.setDossierStatus(PortletConstants.DOSSIER_STATUS_ERROR);
+				toBackOffice.setCompanyId(toEngineMsg.getCompanyId());
+				toBackOffice.setGovAgencyCode(govAgencyCode);
 
 				Message sendToBackOffice = new Message();
 
