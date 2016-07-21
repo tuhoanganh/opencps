20a35a136b69268016263f5eb4f3c95ecde422e9
Khoa Hạnh Phúc <khoavd@fds.vn>
2016-07-21 10:22:45 +0700

    Update - Payment oid - Update Listener MsgOutBackOffice - Update Listener MsgOutFrontOffice

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/backend/exc/MsgInBackOffice.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/backend/exc/MsgInBackOffice.java
@@ -17,11 +17,24 @@
 
 package org.opencps.backend.exc;
 
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.messaging.MessageListenerException;
+
 
 /**
  * @author khoavd
  * 
  */
-public class MsgInBackOffice {
+public class MsgInBackOffice implements MessageListener{
+    @Override
+    public void receive(Message message)
+        throws MessageListenerException {
 
+	    _doReceive(message);
+    }
+    
+    private void _doReceive(Message message) {
+    	System.out.println("**doRevice msgInBackOffice");
+    }
 }
