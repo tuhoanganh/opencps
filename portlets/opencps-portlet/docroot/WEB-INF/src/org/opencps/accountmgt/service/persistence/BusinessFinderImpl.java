f098462988aecbc7bfb53877549814c176b9e83b
Le Tien Dung (FDS) <dunglt@fds.vn>
2016-08-23 15:40:47 +0700

    add BusinessFinderImpl.java

---
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/accountmgt/service/persistence/BusinessFinderImpl.java
@@ -0,0 +1,267 @@
+/**
+* OpenCPS is the open source Core Public Services software
+* Copyright (C) 2016-present OpenCPS community
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+/**
+ * 
+ */
+package org.opencps.accountmgt.service.persistence;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.opencps.accountmgt.model.Business;
+
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.dao.orm.Type;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
+import com.liferay.util.dao.orm.CustomSQLUtil;
+
+
+/**
+ * @author dunglt
+ *
+ */
+public class BusinessFinderImpl extends BasePersistenceImpl<Business> 
+	implements BusinessFinder{
+	
+	public static final String SEARCH_BUSINESS = BusinessFinder.class.getName() + ".searchBusiness";
+	public static final String COUNT_BUSINESS = BusinessFinder.class.getName() + ".countBusiness";
+	
+	
+	public List<Business>  searchBusiness (long groupId, String keywords, int accountStatus,
+		String businessDomain, int start, int end) {
+		String[] names = null;
+		boolean andOperator = false;
+		
+		if (Validator.isNotNull(keywords)) {
+				System.out.println("go here pubsearch");
+				names = CustomSQLUtil
+					.keywords(keywords);
+		} else {
+			andOperator = true;
+		}
+		return _searchBusiness(groupId, names, accountStatus, andOperator,businessDomain, start, end);
+	}
+	
+	private List<Business> _searchBusiness(long groupId, String[] keywords,
+		int accountStatus, boolean andOperator,String businessDomain, int start, int end) {
+		
+		keywords = CustomSQLUtil
+						.keywords(keywords, false);
+		
+		Session session = null;
+		try {
+			session = openSession();
+			String sql = CustomSQLUtil.get(SEARCH_BUSINESS);
+			if(keywords!= null && keywords.length > 0) {
+				
+				System.out.println("GO TO IFFFF and keywords ====== " + keywords.toString());
+				sql = CustomSQLUtil.replaceKeywords(sql, 
+					"lower(opencps_acc_business.name)", StringPool.LIKE, true, keywords);
+				
+				sql = CustomSQLUtil.replaceKeywords(sql, 
+					"lower(opencps_acc_business.shortName))", StringPool.LIKE, true, keywords);
+				
+				sql = CustomSQLUtil.replaceKeywords(sql, 
+					"lower(opencps_acc_business.enName", StringPool.LIKE, true, keywords);
+			} else {
+				
+				System.out.println("GO TO ELSEEEEEEE");
+				
+				
+				sql = StringUtil.replace(sql, 
+					"AND ((lower(opencps_acc_business.name) LIKE [$AND_OR_NULL_CHECK$])" 
+					, StringPool.BLANK);
+				
+				sql = StringUtil.replace(sql, "OR (lower(opencps_acc_business.shortName) LIKE [$AND_OR_NULL_CHECK$])"
+					, StringPool.BLANK);
+				
+				sql = StringUtil.replace(sql, "OR (lower(opencps_acc_business.enName) LIKE [$AND_OR_NULL_CHECK$]))"
+					, StringPool.BLANK);
+			}
+			
+			if(Validator.isNull(businessDomain)) {
+				sql = StringUtil.replace(sql, "AND (opencps_acc_businessdomain.businessDomainCode = ?)", StringPool.BLANK);
+			}
+			
+			System.out.println("*******sql********* " + sql+"  === " );
+			
+			sql = CustomSQLUtil.replaceAndOperator(sql, andOperator);
+			
+			SQLQuery q = session
+							.createSQLQuery(sql);
+			q
+			.setCacheable(false);
+			q
+			.addEntity("Business", Business.class);
+			
+			QueryPos qPos = QueryPos
+							.getInstance(q);
+			qPos
+			.add(groupId);
+			
+			qPos
+			.add(accountStatus);
+			
+			if(Validator.isNotNull(businessDomain)) {
+				qPos.add(businessDomain);
+			}
+			
+			if (keywords != null && keywords.length > 0) {
+				qPos
+					.add(keywords, 2);
+				qPos
+					.add(keywords, 2);
+				
+				qPos
+				.add(keywords, 2);
+				qPos
+				.add(keywords, 2);
+			
+				qPos
+				.add(keywords, 2);
+				qPos
+					.add(keywords, 2);
+			}
+			
+			return (List<Business>) QueryUtil
+							.list(q, getDialect(), start, end);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			session.close();
+		}
+		return null;
+	}
+	
+	public int countBussiness(long groupId, String keywords, int accountStatus,
+		String businessDomain) {
+		
+		String[] names = null;
+		boolean andOperator = false;
+		
+		if (Validator.isNotNull(keywords)) {
+			names = CustomSQLUtil
+				.keywords(keywords);
+		} else {
+			andOperator = true;
+		}
+		
+		return _countBussiness(groupId, names,  accountStatus, businessDomain, andOperator);
+	}
+	
+	private int _countBussiness(long groupId, String[] keywords, int accountStatus,
+		String businessDomain, boolean andOperator) {
+		
+			keywords = CustomSQLUtil
+							.keywords(keywords, false);
+			
+			Session session = null;
+			try {
+				session = openSession();
+				String sql = CustomSQLUtil.get(SEARCH_BUSINESS);
+				if(keywords!= null && keywords.length > 0) {
+					sql = CustomSQLUtil.replaceKeywords(sql, 
+						"lower(opencps_acc_business.name)", StringPool.LIKE,true, keywords);
+					
+					sql = CustomSQLUtil.replaceKeywords(sql, 
+						"lower(opencps_acc_business.shortName))", StringPool.LIKE,true, keywords);
+					
+					sql = CustomSQLUtil.replaceKeywords(sql, 
+						"lower(opencps_acc_business.enName", StringPool.LIKE,true, keywords);
+				} else {
+					sql = StringUtil.replace(sql, 
+						"AND ((lower(opencps_acc_business.name) LIKE [$AND_OR_NULL_CHECK$])" 
+						, StringPool.BLANK);
+					
+					sql = StringUtil.replace(sql, "OR (lower(opencps_acc_business.shortName) LIKE [$AND_OR_NULL_CHECK$])"
+						, StringPool.BLANK);
+					
+					sql = StringUtil.replace(sql, "OR (lower(opencps_acc_business.enName) LIKE [$AND_OR_NULL_CHECK$]))"
+						, StringPool.BLANK);
+				}
+				
+				if(Validator.isNull(businessDomain)) {
+					sql = StringUtil.replace(sql, "AND (opencps_acc_businessdomain.businessDomainCode = ?)", StringPool.BLANK);
+				}
+				
+				SQLQuery q = session
+								.createSQLQuery(sql);
+				q
+				.setCacheable(false);
+				
+				q
+				.addScalar(COUNT_COLUMN_NAME, Type.INTEGER);
+	
+				QueryPos qPos = QueryPos
+				.getInstance(q);
+				
+				qPos
+				.add(groupId);
+				
+				qPos
+				.add(accountStatus);
+				
+				if(Validator.isNotNull(businessDomain)) {
+					qPos.add(businessDomain);
+				}
+				
+				if (keywords != null && keywords.length > 0) {
+					qPos
+						.add(keywords, 2);
+					qPos
+						.add(keywords, 2);
+					
+					qPos
+					.add(keywords, 2);
+					qPos
+					.add(keywords, 2);
+				
+					qPos
+					.add(keywords, 2);
+					qPos
+						.add(keywords, 2);
+				}
+				Iterator<Integer> itr = q
+					.iterate();
+	
+				if (itr
+					.hasNext()) {
+					Integer count = itr
+						.next();
+	
+					if (count != null) {
+						return count
+							.intValue();
+					}
+				}
+				
+		} catch (Exception e) {
+			
+		} finally {
+			session.close();
+		}
+			
+		return 0;
+	}
+}
