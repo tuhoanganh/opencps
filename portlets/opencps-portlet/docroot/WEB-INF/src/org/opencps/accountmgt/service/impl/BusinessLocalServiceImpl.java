691f3fd4d0ba9daa9d387318e52ac3958b566997
Le Tien Dung (FDS) <dunglt@fds.vn>
2016-10-28 13:59:42 +0700

    toi uu businessLocalserviceImpl

--- portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/accountmgt/service/impl/BusinessLocalServiceImpl.java
+++ portlets/opencps-portlet/docroot/WEB-INF/src/org/opencps/accountmgt/service/impl/BusinessLocalServiceImpl.java
@@ -25,8 +25,6 @@ import java.util.List;
 import org.opencps.accountmgt.NoSuchBusinessException;
 import org.opencps.accountmgt.model.Business;
 import org.opencps.accountmgt.model.BusinessDomain;
-import org.opencps.accountmgt.model.impl.BusinessDomainImpl;
-import org.opencps.accountmgt.service.BusinessLocalServiceUtil;
 import org.opencps.accountmgt.service.base.BusinessLocalServiceBaseImpl;
 import org.opencps.util.DLFolderUtil;
 import org.opencps.util.DateTimeUtil;
@@ -101,17 +99,10 @@ public class BusinessLocalServiceImpl extends BusinessLocalServiceBaseImpl {
 		ServiceContext serviceContext)
 		throws SystemException, PortalException {
 
-		Role roleDefault = null;
-
-		try {
-			roleDefault =
+		 Role roleDefault =
 				RoleLocalServiceUtil.getRole(
 					serviceContext.getCompanyId(),
 					WebKeys.CITIZEN_BUSINESS_ROLE_NAME);
-		}
-		catch (Exception e) {
-			_log.info("ROLE CITIZEN IS NULL");
-		}
 
 		long businessId =
 			counterLocalService.increment(Business.class.getName());
@@ -279,9 +270,7 @@ public class BusinessLocalServiceImpl extends BusinessLocalServiceBaseImpl {
 		business = businessPersistence.update(business);
 
 		if (businessDomainCodes != null && businessDomainCodes.length > 0) {
-			if (businessDomainCodes != null && businessDomainCodes.length > 0) {
 				businessDomainLocalService.addBusinessDomains(businessId, businessDomainCodes);
-			}
 		}
 
 		return business;
@@ -313,27 +302,13 @@ public class BusinessLocalServiceImpl extends BusinessLocalServiceBaseImpl {
 		}
 
 		if (fileEntryId > 0) {
-			FileEntry fileEntry = null;
 			try {
-				fileEntry = DLAppServiceUtil.getFileEntry(fileEntryId);
+				FileEntry fileEntry = DLAppServiceUtil.getFileEntry(fileEntryId);
+				DLFolderLocalServiceUtil.deleteFolder(fileEntry.getFolderId());
 			}
 			catch (Exception e) {
 				_log.error(e);
 			}
-			long folderId = 0;
-
-			if (fileEntry != null) {
-				folderId = fileEntry.getFolderId();
-			}
-
-			if (fileEntry != null) {
-				DLAppServiceUtil.deleteFileEntry(fileEntryId);
-			}
-
-			if (fileEntryId > 0) {
-				DLFolderLocalServiceUtil.deleteFolder(folderId);
-			}
-
 		}
 
 		if (mappingOrgId > 0) {
@@ -438,7 +413,9 @@ public class BusinessLocalServiceImpl extends BusinessLocalServiceBaseImpl {
 		}
 		
 		Organization organization = organizationPersistence.findByPrimaryKey(business.getMappingOrganizationId());
-		organization.setName(fullName);
+		organization.setName(fullName +
+				StringPool.OPEN_PARENTHESIS + idNumber +
+				StringPool.CLOSE_PARENTHESIS);
 		organizationPersistence.update(organization);
 		
 		business.setAddress(address);
@@ -476,9 +453,7 @@ public class BusinessLocalServiceImpl extends BusinessLocalServiceBaseImpl {
 				businessDomainPersistence.remove(bdm);
 			}
 		}
-
 		return business;
-
 	}
 
 	public Business updateBusiness(
@@ -554,7 +529,9 @@ public class BusinessLocalServiceImpl extends BusinessLocalServiceBaseImpl {
 		}
 		
 		Organization organization = organizationPersistence.findByPrimaryKey(business.getMappingOrganizationId());
-		organization.setName(fullName);
+		organization.setName(fullName +
+				StringPool.OPEN_PARENTHESIS + idNumber +
+				StringPool.CLOSE_PARENTHESIS);
 		organizationPersistence.update(organization);
 
 		business.setAddress(address);
